2f437f165c9ac5e6ae9e31da33ca1a79a245d128
Owen Kaluza <owen.kaluza@monash.edu>
2022-10-14 13:49:31 +1100

    Fix svg icon bug in map layer
    https://stackoverflow.com/questions/57671995/webpack-4-gives-background-urlobject-module-as-bg-image

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|svg)/,
-        loader: "url-loader?limit=100000"
+        loader: "url-loader?limit=100000&esModule=false"
       },
       {
         // shaders
